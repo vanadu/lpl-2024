e5402e78f22849ef99119f2c0c3cdd4c55687ab8
Van Albert <motorhobo@gmail.com>
2024-12-08 06:32:30 -0500

    2024.12.08 GuideNav responsive adjustments

--- src/components/GuideNav.js
+++ src/components/GuideNav.js
@@ -57,73 +57,76 @@ const GuideNav = () => {
   return (
     <>
       <div className={styles.guide_nav}>
+        <div className={styles.guide_nav_items}>
 
-        <div className={styles.dropdown} tabindex="1">
-          <i className={styles.db2} tabindex="1"></i>
-          <a className={styles.dropbtn}>All Topics</a>
-          {/* About item */}
-          <div className={styles.dropdown_content}>
-            <i className={styles.db3} tabindex="1"></i>
-            <div className={styles.guide_menu_item_block}>
-              <Link href='/patients' 
-                className={styles.mi}>
-                <h2 className={styles.guide_menu_item_text}>About LarPar</h2>
-              </Link>
-              <h3 className={styles.guide_menu_item_subtext}>Everything you need to know about laryngeal paralysis & GOLPP in dogs</h3>
-            </div>
-            {/* Treatment item */}
-            <div className={styles.guide_menu_item_block}>
-              <Link href='/patients/treatment' 
-                className={styles.mi}>
-                <h2 className={styles.guide_menu_item_text}>Treatment Options</h2>
-              </Link>
-              <h3 className={styles.guide_menu_item_subtext}>Treatment options for laryngeal paralysis & GOLPP</h3>
-            </div>
-            {/* Research and Info item */}
-            <div className={styles.guide_menu_item_block}>
-              <Link href='/patients/links' 
-                className={styles.mi}>
-                <h2 className={styles.guide_menu_item_text}>Research &amp; Info</h2>
-              </Link>
-              <h3 className={styles.guide_menu_item_subtext}>Links to research and veterinary websites</h3>
-            </div>
-            {/* DVMs item */}
-            <div className={styles.guide_menu_item_block}>
-              <Link href='/dvms' 
-                className={styles.mi}>
-                <h2 className={styles.guide_menu_item_text}>Stent Info for DVMs</h2>
-              </Link>
-              <h3 className={styles.guide_menu_item_subtext}>Information for DVMs about the stent procedure for Lar Par</h3>
-            </div>
-            {/* News and Notes item */}
-            <div className={styles.guide_menu_item_block}>
-              <Link href='/blog' 
-                className={styles.mi}>
-                <h2 className={styles.guide_menu_item_text}>Lar Par News &amp; Notes</h2>
-              </Link>
-              <h3 className={styles.guide_menu_item_subtext}>News and information from the world of laryngeal paralysis in dogs</h3>
-            </div>
-            {/* Contact & Community */}
-            <div className={styles.guide_menu_item_block}>
-              <Link href='/blog' 
-                className={styles.mi}>
-                <h2 className={styles.guide_menu_item_text}>Contact &amp; Community</h2>
-              </Link>
-              <h3 className={styles.guide_menu_item_subtext}>Ways to contact us and join the international Lar Par community</h3>
-            </div>
+          <div className={styles.dropdown} tabindex="1">
+            <i className={styles.db2} tabindex="1"></i>
+            <a className={styles.dropbtn}>All Topics</a>
+            {/* About item */}
+            <div className={styles.dropdown_content}>
+              <i className={styles.db3} tabindex="1"></i>
+              <div className={styles.guide_menu_item_block}>
+                <Link href='/patients' 
+                  className={styles.mi}>
+                  <h2 className={styles.guide_menu_item_text}>About LarPar</h2>
+                </Link>
+                <h3 className={styles.guide_menu_item_subtext}>Everything you need to know about laryngeal paralysis & GOLPP in dogs</h3>
+              </div>
+              {/* Treatment item */}
+              <div className={styles.guide_menu_item_block}>
+                <Link href='/patients/treatment' 
+                  className={styles.mi}>
+                  <h2 className={styles.guide_menu_item_text}>Treatment Options</h2>
+                </Link>
+                <h3 className={styles.guide_menu_item_subtext}>Treatment options for laryngeal paralysis & GOLPP</h3>
+              </div>
+              {/* Research and Info item */}
+              <div className={styles.guide_menu_item_block}>
+                <Link href='/patients/links' 
+                  className={styles.mi}>
+                  <h2 className={styles.guide_menu_item_text}>Research &amp; Info</h2>
+                </Link>
+                <h3 className={styles.guide_menu_item_subtext}>Links to research and veterinary websites</h3>
+              </div>
+              {/* DVMs item */}
+              <div className={styles.guide_menu_item_block}>
+                <Link href='/dvms' 
+                  className={styles.mi}>
+                  <h2 className={styles.guide_menu_item_text}>Stent Info for DVMs</h2>
+                </Link>
+                <h3 className={styles.guide_menu_item_subtext}>Information for DVMs about the stent procedure for Lar Par</h3>
+              </div>
+              {/* News and Notes item */}
+              <div className={styles.guide_menu_item_block}>
+                <Link href='/blog' 
+                  className={styles.mi}>
+                  <h2 className={styles.guide_menu_item_text}>Lar Par News &amp; Notes</h2>
+                </Link>
+                <h3 className={styles.guide_menu_item_subtext}>News and information from the world of laryngeal paralysis in dogs</h3>
+              </div>
+              {/* Contact & Community */}
+              <div className={styles.guide_menu_item_block}>
+                <Link href='/blog' 
+                  className={styles.mi}>
+                  <h2 className={styles.guide_menu_item_text}>Contact &amp; Community</h2>
+                </Link>
+                <h3 className={styles.guide_menu_item_subtext}>Ways to contact us and join the international Lar Par community</h3>
+              </div>
 
+            </div>
           </div>
-        </div>
 
-        {/* can't use a Link component of course, because the href drives to a different page before the router.back is called */}
-        <div className={styles.guide_nav_back}>
-          <div
-            onClick={doOnClick}
-            className={styles.guide_nav_back_link}>
-              <TfiBackRight 
-                className={styles.guide_nav_back_icon}
-              />
-              Back 
+
+          {/* can't use a Link component of course, because the href drives to a different page before the router.back is called */}
+          <div className={styles.guide_nav_back}>
+            <div
+              onClick={doOnClick}
+              className={styles.guide_nav_back_link}>
+                <TfiBackRight 
+                  className={styles.guide_nav_back_icon}
+                />
+                Back 
+            </div>
           </div>
         </div>
       </div>
