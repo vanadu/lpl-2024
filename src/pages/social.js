3f525dbc9d1b83b9eeb2fea91bc28158e56c8492
Van Albert <motorhobo@gmail.com>
2024-12-21 07:06:55 -0500

    2024.12.22 Social page formatting and links

--- src/pages/social.js
+++ src/pages/social.js
@@ -28,6 +28,8 @@ const Social = () => {
             The Lar Par community is the collective of people whose lives have been impacted by canine laryngeal paralysis/GOLPP. 
           </p>
 
+
+
           {/* ShowMoreComponent content start */}
           <div className={styles.content_text}>
             <ShowMoreContent
@@ -55,7 +57,6 @@ const Social = () => {
 
 
 
-
           <h2 className={styles.content_head}>The Open Community</h2>
           <p className={styles.content_text}>
             A truly open community embraces and respects all members regardless of their personal circumstances. 
@@ -77,7 +78,7 @@ const Social = () => {
                 </figure> */}
                 <span className="showmore-content-space"></span>
                 <p className="showmore-content-text">
-                  Each community member&rsquo;s dog is unique and individual, and each community member has a unique situation that informs their decisions. For some of us, our personal circumstances sadly limit our available options. A true community accepts this with compassion and understanding and without judgment. There a lot of Facebook groups that deal with canine laryngeal paralysis and GOLPP, but three stand out as being open, unbiased, and supportive of free speech.
+                  Each community member&rsquo;s dog is unique and individual, and each community member has a unique situation that informs their decisions. For some of us, our personal circumstances sadly limit our available options for helping our beloved companions with Lar Par. A true community accepts this with compassion and understanding and without judgment. 
                 </p>
               </div>
             </ShowMoreContent>
@@ -87,48 +88,102 @@ const Social = () => {
 
           <h2 className={styles.content_head}>The Lar Par Community Network</h2>
           <p className={styles.content_text}>
-            The Lar Par Community network has three Facebook groups active in English and German to serve the international Lar Par community:
+            There a lot of Facebook groups that deal with canine laryngeal paralysis and GOLPP, but three stand out as being open, unbiased, and supportive of free speech.
           </p>
-          <h3 className={styles.content_subhead}>Lar Par/GOLPP International Facebook Group</h3>
           <p className={styles.content_text}>
-            This open public group is a positive environment that supports free speech for all people with Lar Par dogs, regardless of personal circumstances.  
-            Learn more...
+            The Lar Par Community network currently has three Facebook groups active in English and German to help people whose dogs suffer from laryngeal paralysis:
           </p>
+          <div className={styles.section_content}>
+              <hr className={styles.content_list_spacer}/>
+          </div>
           <div className={styles.social_links}> 
             <div className={styles.social_link}>
-              <div className={styles.social_link_item}>
-                <Link href='#' className='link-dark'>
-                  <FaFacebook
-                    className={styles.social_icon}
-                  />
-                  <p className={styles.social_icon_caption}>
-                    Lar Par/GOLPP International Group (English language)
+              <div className={styles.social_link_head}>
+                <h3 className={styles.social_link_head_text}>Lar Par/GOLPP International <span className='mobile-show-inline'><br /></span> Facebook Group</h3>
+              </div>
+              <div className={styles.social_link_content}>
+                <p className={styles.social_link_content_text}>
+                  This open English language group is a positive environment that supports free speech for all people with Lar Par dogs, regardless of personal circumstances.  
+                  Learn more...
+                </p>
+                <div className={styles.social_link_item}>
+                  <Link 
+                    href='https://www.facebook.com/groups/1737118503207596' 
+                    className='link-dark' 
+                    rel="noopener noreferrer" 
+                    target="_blank">
+                    <FaFacebook
+                      className={styles.social_icon}
+                    />
+                    <p className={styles.social_icon_caption}>
+                      Lar Par/GOLPP International Group <span className='mobile-show-inline'><br /></span> (English language)
+                    </p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className={styles.section_content}>
+              <hr className={styles.content_list_spacer}/>
+            </div>
+
+            <div className={styles.social_links}> 
+              <div className={styles.social_link}>
+                <div className={styles.social_link_head}>
+                  <h3 className={styles.social_link_head_text}>Kehlkopflähmung / Polyneuropathie beim Hund  - Erfahrungen und Austausch</h3>
+                </div>
+                <div className={styles.social_link_content}>
+                  <p className={styles.social_link_content_text}>
+                    Diese deutschsprachige Gruppe ist eine offene, positive Umgebung für den Austausch von Informationen und Erfahrungen zum Thema Kehlkopflähmung und GOLPP. 
                   </p>
-                </Link>
-              </div>
-            </div>
-            <div className={styles.social_link}>
-              <div className={styles.social_link_item}>
-                <Link href='#' className='link-dark'>
-                  <FaFacebook
-                    className={styles.social_icon}
-                  />
-                  Diese Gruppe ist eine oeffentliche, positive Umgebung zum offenen Austausch von Informationen und Erfahrung in bezug auf Kehlkopflaehmung und GOLPP. 
-                  Learn more...  
-                </Link>
-              </div>
-            </div>
-            <div className={styles.social_link}>
-              <div className={styles.social_link_item}>
-                <Link href='#' className='link-dark'>
-                  <FaFacebook
-                    className={styles.social_icon}
-                  />
-                  This group was founded in 2022 to advocate for and inform about the safe stent alternative for canine laryngeal paralysis, because information about this procedure continues to be suppressed in other Lar Par Facebook groups and the veterinary establishment.
-                </Link>
+                  <div className={styles.social_link_item}>
+                    <Link 
+                      href='https://www.facebook.com/groups/1424122448476490/' 
+                      className='link-dark' 
+                      rel="noopener noreferrer" 
+                      target="_blank">
+                      <FaFacebook
+                        className={styles.social_icon}
+                      />
+                      <p className={styles.social_icon_caption}>
+                        Lar Par/GOLPP International Group <span className='mobile-show-inline'><br /></span>(German language)
+                      </p>
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
 
+            <div className={styles.section_content}>
+              <hr className={styles.content_list_spacer}/>
+            </div>
+
+            <div className={styles.social_link}>
+              <div className={styles.social_link_head}>
+                <h3 className={styles.social_link_head_text}>Laryngeal Stent Treatment <span className='mobile-show-inline'><br /></span> for Laryngeal Paralysis (GOLPP)</h3>
+              </div>
+              <div className={styles.social_link_content}>
+                <p className={styles.social_link_content_text}>
+                  This group was founded in 2022 to advocate for and inform about the silicone stent alternative for canine laryngeal paralysis, because information about this procedure continues to be suppressed in many Facebook groups for canine laryngeal paralysis and the veterinary establishment.
+                </p>
+                <div className={styles.social_link_item}>
+                  <Link 
+                    href='https://www.facebook.com/groups/laryngealstentfordogs' 
+                    className='link-dark' 
+                    rel="noopener noreferrer" 
+                    target="_blank">
+                    <FaFacebook
+                      className={styles.social_icon}
+                    />
+                    <p className={styles.social_icon_caption}>
+                    Laryngeal Stent Treatment for <span className='mobile-show-inline'><br /></span> Laryngeal Paralysis (GOLPP) 
+                    </p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className={styles.section_content}>
+              <hr className={styles.content_list_spacer}/>
+            </div>
           </div>
         </div>
       </section>
